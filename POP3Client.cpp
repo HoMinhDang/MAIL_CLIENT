b82806cbf4a9c1a4e776f1a9b26dfe510a36efa1
HoMinhDang <minhdang202044@gmail.com>
2023-11-18 16:30:32 +0700

    Add func login

--- POP3Client.cpp
+++ POP3Client.cpp
@@ -1,88 +1,33 @@
 #include "POP3Client.h"
 
-std::string POP3Client::receiveMultilineResponse() const
-{
-    std::string response;
-    do{
-        response += receiveResponse();
-
-    }while(response.find("\r\n.\r\n") == std::string::npos);
-    return response;
-}
 
 bool POP3Client::login(const std::string& username, const std::string& password)
 {
+    // Connect to the server
     if (!connectToServer())
-        return false;
-
-    // username
-    receiveResponse(); // first message
-    sendCommand("USER " + username + "\r\n");
-    receiveResponse();
-
-    // password
-    sendCommand("PASS " + password + "\r\n");
-    std::string pass_response = receiveResponse(); // Receive response to PASS command
-
-    return pass_response.find("+OK") != std::string::npos;
-}
-
-std::vector<std::string> POP3Client::listEmail()
-{
-    sendCommand("LIST\r\n");
-    std::string list_response = receiveMultilineResponse(); // receive response
-
-    std::vector<std::string> list_email;
-    std::istringstream response_stream(list_response);
-    std::string line;
-
-    // skip first line
-    std::getline(response_stream, line);
-
-    while (std::getline(response_stream, line))
     {
-        std::istringstream line_stream(line);
-        std::string email_ID;
-        line_stream >> email_ID;
-
-        // check is this email in downloaded 
-        if (downloaded_email.find(email_ID) == downloaded_email.end())
-        {
-            downloaded_email.insert(email_ID);
-            list_email.push_back(email_ID);
-        }
+        std::cerr << "Failed to connect to the server.\n";
+        return false;
     }
-    return list_email;
+
+    // Send the USER command
+    sendCommand("USER " + username + "\r\n");
+    std::string userResponse = receiveResponse();  // Receive and store the response
+    if (userResponse.substr(0, 3) != "+OK")
+    {
+        std::cerr << "Failed to send USER command\n";
+        return false;
+    }
+
+    // Send the PASS command
+    sendCommand("PASS " + password + "\r\n");
+    std::string passResponse = receiveResponse();  // Receive and store the response
+    if (passResponse.substr(0, 3) != "+OK")
+    {
+        std::cerr << "Failed to login\n";
+        return false;
+    }
+
+    return true;
 }
 
-std::string POP3Client::retrieveEmail(int email_ID)
-{
-    sendCommand("RETR " + std::to_string(email_ID) + "\r\n");
-    return receiveMultilineResponse();
-}
-
-std::string POP3Client::getMailBoxStatus()
-{
-    sendCommand("STAT\r\n");
-    return receiveResponse();
-}
-
-bool POP3Client::markForDeletion(int email_ID)
-{
-    sendCommand("DELE " + std::to_string(email_ID) + "\r\n");
-    std::string response = receiveResponse();
-    return response.find("+OK") != std::string::npos;
-}
-
-bool POP3Client::resetSession()
-{
-    sendCommand("RSET\r\n");    
-    std::string response = receiveResponse();
-    return response.find("+OK") != std::string::npos;
-}
-
-void POP3Client::quitSession()
-{
-    sendCommand("QUIT\r\n");
-    receiveResponse(); // Receive response to QUIT command
-}
--- POP3Client.h
+++ POP3Client.h
@@ -2,6 +2,7 @@
 #include <set>
 #include <vector>
 #include <sstream>
+#include "Email.h"
 
 
 #ifndef POP3_CLIENT_H
@@ -10,40 +11,14 @@
 class POP3Client : public Client
 {
 private:
-    std::set<std::string> downloaded_email;
-
-    //recive multiline response
-    std::string receiveMultilineResponse() const;
+    std::set<Email> downloaded_email;
 
 public:
     POP3Client(const std::string& server_address, int server_port)
         : Client(server_address, server_port){}
     ~POP3Client(){}
 
-    // log in to POP3 server
     bool login(const std::string& username, const std::string& password);
-
-    // list emails on the server
-    std::vector<std::string> listEmail();
-
-    // retrieve Email
-    std::string retrieveEmail(int email_ID);
-
-    // get status of mailbox
-    std::string getMailBoxStatus();
-
-    // mark email for deletion 
-    bool markForDeletion(int email_ID);
-
-    // reset session
-    bool resetSession();
-
-    //  quit session
-    void quitSession();
-
-    // load and save set downloaded mail
-    void loadDownloadedEmails();
-    void saveDownloadedEmails(const std::set<std::string>& downloadedEmails);
 
 };
 
