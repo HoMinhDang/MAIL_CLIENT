8d8e1ebe954d6fa76f04374b97bcd3bca56f119d
HoMinhDang <minhdang202044@gmail.com>
2023-11-18 15:52:26 +0700

    Add WinsockManager.h that create only one WSA throught application

---
+++ WinsockManager.h
@@ -0,0 +1,18 @@
+// WinsockManager.h
+#ifndef WINSOCK_MANAGER_H
+#define WINSOCK_MANAGER_H
+
+#include <winsock2.h>
+
+class WinsockManager {
+private:
+    WSADATA wsa_data;
+
+    WinsockManager();
+    ~WinsockManager();
+
+public:
+    static WinsockManager& getInstance();
+};
+
+#endif // WINSOCK_MANAGER_H
