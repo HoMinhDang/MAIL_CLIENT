ae091f5ecd57ab26d02f4b54b0ea69ccc7279deb
HoMinhDang <minhdang202044@gmail.com>
2023-11-26 01:18:29 +0700

    Add save file

--- SaveObject.cpp
+++ SaveObject.cpp
@@ -7,11 +7,12 @@ namespace fs = std::filesystem;
 
 namespace fs = std::filesystem;
 
-void saveEmail(const std::string& username, const std::string& email_content, const std::string& filename)
+void saveEmail(const std::string& username, const std::string& email_content, const std::string& filename, const std::string& subfolder)
 {
     std::string database_folder = "database";
     std::string user_folder = database_folder + "/" + username;
-    std::string file_path = user_folder + "/" + filename;
+    std::string deeper_folder = user_folder + "/" + subfolder;  
+    std::string file_path = deeper_folder + "/" + filename;
 
     if (!fs::exists(database_folder))
     {
@@ -21,6 +22,11 @@ void saveEmail(const std::string& username, const std::string& email_content, co
     if (!fs::exists(user_folder))
     {
         fs::create_directory(user_folder);
+    }
+
+    if (!fs::exists(deeper_folder))
+    {
+        fs::create_directory(deeper_folder);
     }
 
     std::ofstream file(file_path);
@@ -33,6 +39,20 @@ void saveEmail(const std::string& username, const std::string& email_content, co
     else
     {
         std::cerr << "Error saving email in folder: " << file_path << "\n";
+    }
+}
+
+void saveFile(const std::string& file_path, const std::string& filename, const std::string& file_content)
+{
+    std::vector<BYTE> decoded_content = base64_decode(file_content);
+
+    std::ofstream file(file_path + "/" + filename, std::ios::binary);
+    if (file.is_open()) {
+        file.write(reinterpret_cast<const char*>(decoded_content.data()), decoded_content.size());
+        file.close();
+        std::cout << "File saved successfully: " << file_path << "/" << filename << std::endl;
+    } else {
+        std::cerr << "Error: Unable to open file for writing: " << file_path << "/" << filename << std::endl;
     }
 }
 
--- SaveObject.h
+++ SaveObject.h
@@ -1,11 +1,11 @@
-
+#include "base64.h"
 #ifndef SAVE_OBJECT_H
 #define SAVE_OBJECT_H
 
 #include <string>
 
 
-    void saveEmail(const std::string& username, const std::string& email_content, const std::string& filename);
-    void saveFile(const std::string& file_path, const std::string& file_content);
+    void saveEmail(const std::string& username, const std::string& email_content, const std::string& filename, const std::string& subfolder);
+    void saveFile(const std::string& file_path, const std::string& filename, const std::string& file_content);
 
 #endif // SAVE_OBJECT_H
